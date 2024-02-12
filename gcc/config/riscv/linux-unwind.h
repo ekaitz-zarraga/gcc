1fccf02ad5587b8d10518a50eb528ca41f626982
Ekaitz Zarraga <ekaitz@elenq.tech>
2024-02-12 14:05:15 +0100

    EMPTY: ucontext_t issues explanation:
    Since 883312dc79806f513275b72502231c751c14ff72 gcc doesn't use `struct
    ucontext` but we may need for the bootstrapping. If we are able to build
    the other parts of the compiler that use that, we'll probably need to
    revert this in the RISC-V part too.
    
    I was able to build the RISC-V backend because we have the modern
    approach rather than the old one, this might produce other problems
    during the bootstrapping.
    
    See also: a33c50fd788

--- gcc/config/riscv/linux-unwind.h
+++ gcc/config/riscv/linux-unwind.h
@@ -42,7 +42,7 @@ riscv_fallback_frame_state (struct _Unwind_Context *context,
   struct rt_sigframe
   {
     siginfo_t info;
-    ucontext_t uc;
+    ucontext_t uc; // TODO: Careful here
   };
 
   struct rt_sigframe *rt_;
